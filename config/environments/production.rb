33dbafe6b2b01b4ea42d4acdf2f8a6cf74a1c408
theponti <cjponti@gmail.com>
2013-07-19 08:54:18 +0100

    Updated aws region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ GeneralAssembly::Application.configure do
         :provider              => 'AWS',
         :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
         :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+        :region                => 'us-east-1'
       }
       config.fog_directory  = ENV['S3_BUCKET_NAME']
   end
