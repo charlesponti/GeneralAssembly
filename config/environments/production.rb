b7424c859a3622ef4471c1a96028f882c9ad0f31
theponti <cjponti@gmail.com>
2013-07-19 09:02:55 +0100

    Fixed typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ GeneralAssembly::Application.configure do
       config.fog_credentials = {
         :provider              => 'AWS',
         :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
         :region                => 'us-east-1'
       }
       config.fog_directory  = ENV['S3_BUCKET_NAME']
